006af59ef9a62df9b4578ad26f6321bb83435f43
AIL1163 <kame_1221@outlook.jp>
2020-01-08 23:11:30 +0900

    DirectX11App ( UWP ) Created for 2020 / 01 / 08.

---
+++ MS_DirectX11_StudyApp/MS_DirectX11_StudyApp/MS_DirectX11_StudyAppMain.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Direct2D および 3D コンテンツを画面上でレンダリングします。
+namespace MS_DirectX11_StudyApp
+{
+	class MS_DirectX11_StudyAppMain : public DX::IDeviceNotify
+	{
+	public:
+		MS_DirectX11_StudyAppMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~MS_DirectX11_StudyAppMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// デバイス リソースへのキャッシュされたポインター。
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: これを独自のコンテンツ レンダラーで置き換えます。
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// ループ タイマーをレンダリングしています。
+		DX::StepTimer m_timer;
+	};
+}
